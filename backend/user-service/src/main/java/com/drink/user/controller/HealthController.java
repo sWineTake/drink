89405b520ca6aeccd02739c09c6a5a14ed780d8a
twocowsong <woosong0215@naver.com>
2026-01-30 19:40:15 +0900

    [INFRA] feat: K8S ConfigMap 설정

--- backend/user-service/src/main/java/com/drink/user/controller/HealthController.java
+++ backend/user-service/src/main/java/com/drink/user/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.drink.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
+    @Value("${MY_ACCOUNT:default}")
+    private String myAccount;
+
     @GetMapping
     public String health() {
-        return "ok - version - 1.1";
+        return "ok - version - 1.1 / MY_ACCOUNT : " + myAccount;
     }
 
 }
